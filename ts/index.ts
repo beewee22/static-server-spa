8d5334a910f865a85e843578d03a55ad042c7645
beewee22 <>
2019-10-02 14:59:34 +0900

    helmet을 이용한 hsts 추가

--- ts/index.ts
+++ ts/index.ts
@@ -1,14 +1,28 @@
-import * as Koa from "koa";
-import * as Serve from "koa-static";
-import * as Send from "koa-send";
+import * as Koa from 'koa';
+import * as Serve from 'koa-static';
+import * as Send from 'koa-send';
+import * as helmet from 'koa-helmet';
 
 const koa = new Koa();
 const port = process.env.PORT || 80;
-const dir = `${__dirname}/../${process.env.SERVE_DIR || "dist"}`;
+const dir = `${__dirname}/../${process.env.SERVE_DIR || 'dist'}`;
+
+koa.use(helmet());
+
+//#region HSTS
+const sixtyDaysInSeconds = 5184000;
+koa.use(
+  helmet.hsts({
+    maxAge: sixtyDaysInSeconds,
+    includeSubDomains: true,
+    preload: true,
+  })
+);
+//#endregion
 
 koa.use(Serve(dir));
 koa.use(async ctx => {
-  if (ctx.status === 404) await Send(ctx, "index.html", { root: dir });
+  if (ctx.status === 404) await Send(ctx, 'index.html', { root: dir });
 });
 
 koa.listen(port);
